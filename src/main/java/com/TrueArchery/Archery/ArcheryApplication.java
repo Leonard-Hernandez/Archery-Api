3c8c4a19da195542b32250f8abbc5b0408f4f1a1
Leonard <Leonardhernandez02@gmail.com>
2024-05-12 19:31:55 -0500

    arreglando tema de credenciales publicas

--- src/main/java/com/TrueArchery/Archery/ArcheryApplication.java
+++ src/main/java/com/TrueArchery/Archery/ArcheryApplication.java
@@ -3,7 +3,8 @@ package com.TrueArchery.Archery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.TrueArchery.Archery"})
+
 public class ArcheryApplication {
 
 	public static void main(String[] args) {
